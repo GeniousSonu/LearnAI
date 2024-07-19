9d1f22a0bcdb6d195521f06534756a5f60c4ab6f
SK SAHINUR ISLAM <67267702+GeniousSonu@users.noreply.github.com>
2024-07-19 21:20:46 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+document.querySelector('.theme-toggle.yellow').addEventListener('click', () => {
+    document.body.style.backgroundColor = '#2c3e50';
+    document.body.style.color = '#ecf0f1';
+  });
+  
+  document.querySelector('.theme-toggle.gray').addEventListener('click', () => {
+    document.body.style.backgroundColor = '#7f8c8d';
+    document.body.style.color = '#ecf0f1';
+  });
+  
